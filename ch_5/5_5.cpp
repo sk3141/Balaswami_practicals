48b243e174a3361129fce8ecf4eb32d0e8fc1780
sahilkaundal <sahil21052003@gmail.com>
2023-11-14 15:50:27 +0530

    14/11/23

--- ch_5/5_5.cpp
+++ ch_5/5_5.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
+//#include <cmath>
 using namespace std;
 class DM
 {
     public:
-    float meter;
-    float centimeter;
+    int meter;
+    int centimeter;
 
     DB Convert(DM& temp)
     {
         DB res;
-        res.feet = temp.meter / 0.3 + temp.centimeter / 30;
-        res.inches = (temp.centimeter % 30) / 2.54;
+        int t = 0;
+        t = temp.meter * 100 + temp.centimeter;
+        res.feet = t / 30;
+        res.inches = (t % 30) / 2.54;
         return res;
     }
 };
 class DB
 {
     public:
-    float feet;
-    float inches;
+    int feet;
+    int inches;
 
     DM Convert(const DB& temp)
     {
-        DM res
+        DM res;
+        int t = 0;
+        t = temp.feet * 12 + temp.inches;
+        res.meter = t / 39.3;
+        res.centimeter = t * 2.54;
+        return res;
     }
 };
 
