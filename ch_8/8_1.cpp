0b60fb305e5cea0cab147b6cf4fe49d1e910cf96
sahilkaundal <sahil21052003@gmail.com>
2023-11-14 09:48:25 +0530

    Adding Files

--- ch_8/8_1.cpp
+++ ch_8/8_1.cpp
@@ -1,1 +1,52 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Account
+{
+    protected:
+    int accNumber;
+    float accBalance;
+    string depositorName;
+
+    public:
+    Account(string dName)
+    {
+        depositorName = dName;
+    }
+    float getBalance()
+    {
+        return accBalance;
+    }
+    void deposit(float amount)
+    {
+        accBalance += amount;
+    }
+    void withdraw(float amount)
+    {
+        accBalance -= amount;
+    }
+};
+
+class SavingAccount : public Account
+{
+    public:
+    SavingAccount(string dName) : Account(dName)
+    {
+        depositorName = dName;
+    }
+    void addInterest(float rate)
+    {
+        accBalance += (accBalance * rate) / 100;
+    }
+};
+
+class CurrentAccount : public Account
+{
+
+};
+
+int main()
+{
+    return 0;
+}
