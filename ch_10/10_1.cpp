5d192121cbf3b9d44deb039f755d5915ddca24e0
sahilkaundal <sahil21052003@gmail.com>
2023-11-14 19:54:27 +0530

    14/11/23

---
+++ ch_10/10_1.cpp
@@ -0,0 +1,58 @@
+/*#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    char name[50][50];
+    int code[50];
+    float cost[50];
+
+    cout << "Enter number of records to be entered :- ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "(Record" << i + 1 << ") " << "Name :- ";
+        cin >> name[i];
+        cout << "(Record" << i + 1 << ") " << "Code :- ";
+        cin >> code[i];
+        cout << "(Record" << i + 1 << ") " << "Cost :- ";
+        cin >> cost[i];
+    }
+
+    cout << "\n\n Inputted Details\n\n";
+
+    return 0;
+}*/
+#include <iostream>
+#include <iomanip>  // For setw
+void printSeparatorLine(int width) {
+    for (int i = 0; i < width; ++i) {
+        std::cout << "-";
+    }
+    std::cout << std::endl;
+}
+int main() {
+    // Table headers
+    std::cout << std::setw(15) << std::left << "Name";
+    std::cout << std::setw(10) << std::left << "Code";
+    std::cout << std::setw(10) << std::left << "Cost" << std::endl;
+
+    printSeparatorLine(30);
+    // Table data
+    std::cout << std::setw(15) << std::left << "Turbo C++";
+    std::cout << std::setw(10) << std::left << 1000;
+    std::cout << std::setw(10) << std::left << 300.25 << std::endl;
+
+    std::cout << std::setw(15) << std::left << "DEV C++";
+    std::cout << std::setw(10) << std::left << 500;
+    std::cout << std::setw(10) << std::left << 5000.00 << std::endl;
+
+    std::cout << std::setw(15) << std::left << "MSVC";
+    std::cout << std::setw(10) << std::left << 1001;
+    std::cout << std::setw(10) << std::left << 10.00 << std::endl;
+
+    return 0;
+}
