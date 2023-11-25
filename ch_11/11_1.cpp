254dce4d0960a5ebe83aeafc155aac1f88c9a7d9
sahil <sahil21052003@gmail.com>
2023-11-25 13:46:49 +0530

    25/11/23

--- ch_11/11_1.cpp
+++ ch_11/11_1.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <string>
 
+int main()
+{
+    std::ifstream input("input.txt");
+    if (!input)
+    {
+        std::cerr << "Cannot open input.txt" << std::endl;
+        return 1;
+    }
 
+    std::ofstream output("output.txt");
+    if (!output)
+    {
+        std::cerr << "Cannot open output.txt" << std::endl;
+        return 1;
+    }
 
+    std::string line;
+
+    while(std::getline(input, line))
+    {
+        bool previousIsSpace = false;
+        for (int i = 0; line[i] != '\0'; i++)
+        {
+            char c = line[i];
+            if (c == ' ')
+            {
+                if (!previousIsSpace)
+                {
+                    output << c;                    
+                }
+                previousIsSpace = true;
+            }
+            else
+            {
+                output << c;
+                previousIsSpace = false;
+            }
+        }
+        output << '\n';
+    }
+    input.close();
+    output.close();
+    return 0;
+}
